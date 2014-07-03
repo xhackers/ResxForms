01e24284b07b3dfa3a7cadd06121b86723fd690c
hieumoscow89 <hieumoscow89@yahoo.com>
2014-07-03 08:31:35 +0700

    First Commit

---
+++ LocalizationForm/LocalizationForm/App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LocalizationForm.AppResources;
+using Xamarin.Forms;
+
+namespace LocalizationForm
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {
+            return new ContentPage
+            {
+                Content = new Label
+                {
+                    Text = LocalizationResources.Sth,
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                },
+            };
+        }
+    }
+}
---
+++ LocalizationForm/LocalizationForm/AppResources/LocalizedStrings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalizationForm.AppResources
+{
+    public class LocalizedStrings
+    {
+        private static readonly LocalizationResources _localizedResources = new LocalizationResources();
+
+        public LocalizationResources LocalizationResources { get { return _localizedResources; } }
+    }
+}
